dc00c4f37618cb075ba130a4edae14330c51f89e
meeyth <anurupbhowmick2003@gmail.com>
2024-02-26 00:10:37 +0530

    commit #1

---
+++ README.md
@@ -0,0 +1,30 @@
+# Message Spam Classifier Web App
+
+This is a web application built using Streamlit, which allows users to classify messages as spam or not spam (ham). The application utilizes a machine learning model trained on the NLTK, scikit-learn, NumPy, and Pandas libraries to perform the classification.
+
+## Features
+
+- **Message Classification:** Users can input a message, and the application will predict whether the message is spam or not.
+- **Interactive User Interface:** The application provides a user-friendly interface built with Streamlit, allowing users to interact with the classifier effortlessly.
+- **Real-time Predictions:** Messages are classified in real-time, providing instant feedback to users.
+
+## Dependencies
+
+To run this application locally, you need to install the following dependencies:
+
+- Streamlit
+- streamlit_lottie
+- NLTK
+- scikit-learn
+- NumPy
+- Pandas
+
+You can install these dependencies using the following command:
+
+```bash
+pip install streamlit streamlit_lottie nltk scikit-learn numpy pandas
+```
+
+## Preview
+
+![Preview](preview.png)
---
+++ app.py
@@ -0,0 +1,75 @@
+import streamlit as st
+from streamlit_lottie import st_lottie
+import pickle
+import string
+from nltk.corpus import stopwords
+import nltk
+from nltk.stem.porter import PorterStemmer
+
+st.set_page_config(page_title="SMC", layout="wide")
+
+# Run the following line only once
+# nltk.download('punkt')
+
+ps = PorterStemmer()
+
+
+lottie_url = "https://lottie.host/bca2fd02-f316-4777-a2d3-4fba17033373/RG0maEkhLT.json"
+
+
+def transform_text(text):
+    text = text.lower()
+    text = nltk.word_tokenize(text)
+
+    y = []
+    for i in text:
+        if i.isalnum():
+            y.append(i)
+
+    text = y[:]
+    y.clear()
+
+    for i in text:
+        if i not in stopwords.words('english') and i not in string.punctuation:
+            y.append(i)
+
+    text = y[:]
+    y.clear()
+
+    for i in text:
+        y.append(ps.stem(i))
+
+    return " ".join(y)
+
+
+tfidf = pickle.load(open('vectorizer.pkl', 'rb'))
+model = pickle.load(open('model.pkl', 'rb'))
+
+st.title("Spam Message Classifier 🛸")
+
+
+with st.container():
+    st.write("---")
+
+    left, right = st.columns(2)
+    with left:
+        input_sms = st.text_area("Enter the message")
+        if st.button('Predict'):
+
+            # 1. preprocessing
+            transformed_sms = transform_text(input_sms)
+
+            # 2. vectorization
+            vector_input = tfidf.transform([transformed_sms])
+
+            # 3. predicting
+            result = model.predict(vector_input)[0]
+
+            # 4. Displaying the prediction
+            if result == 1:
+                st.header("Spam")
+            else:
+                st.header("Not Spam")
+
+    with right:
+        st_lottie(lottie_url, height=300, key="mail-animation")
